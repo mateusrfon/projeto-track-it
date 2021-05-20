306c7d1074dd869b2e2aa0553bf6e0618c67e8f4
Mateus Ribeiro <mateusrfon@gmail.com>
2021-05-20 00:10:08 -0300

    layout historico

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,25 @@
 import ScreeBody from './ScreenBody';
+import styled from 'styled-components';
 
 export default function History() {
     return (
-        <ScreeBody></ScreeBody>
+        <ScreeBody>
+            <Title>Histórico</Title>
+            <Description>
+                Em breve você poderá ver o histórico dos seus hábitos aqui!
+            </Description>
+        </ScreeBody>
     );
 }
+
+const Title = styled.h1`
+    margin-top: 28px;
+    margin-bottom: 17px;
+    font-size: 23px;
+    color: #126BA5;
+`;
+
+const Description = styled.p`
+    font-size: 18px;
+    color: #666;
+`;
