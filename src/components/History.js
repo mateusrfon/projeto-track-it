f19920137db71a6e6616034c8744cd8041fb33e7
Mateus Ribeiro <mateusrfon@gmail.com>
2021-05-21 20:27:48 -0300

    calendario nao funcional

--- src/components/History.js
+++ src/components/History.js
@@ -1,13 +1,39 @@
 import ScreeBody from './ScreenBody';
 import styled from 'styled-components';
+import { useEffect, useContext, useState } from 'react';
+import UserContext from '../contexts/UserContext';
+import axios from 'axios';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function History() {
+    const { token } = useContext(UserContext);
+    const [days, setDays] = useState([]);
+
+    useEffect(() => {
+        const config = {
+            headers: {
+                Authorization: token
+            }
+        }
+        const require = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/history/daily', config);
+        require.then(response => {
+            const data = response.data;
+            const newData = [];
+            for (let i = 0; i < data.length; i++) {
+                newData.push({ data: data[i].day, done: data[i].habits.reduce((acc,e) => (acc && e.done), true) });
+            }
+            setDays(newData);
+        });
+    }, []);
+
     return (
         <ScreeBody>
             <Title>Histórico</Title>
             <Description>
                 Em breve você poderá ver o histórico dos seus hábitos aqui!
             </Description>
+            <Calendar />
         </ScreeBody>
     );
 }
@@ -22,4 +48,5 @@ const Title = styled.h1`
 const Description = styled.p`
     font-size: 18px;
     color: #666;
+    margin-bottom: 17px;
 `;
